db568b1b4f4e141d5264c7f2b89325c80b6e642f
DlopezS98 <01dlopezs98@gmail.com>
2025-04-29 13:04:43 -0600

    Add product creation form and submission handling

---
+++ admin/products/create.js
@@ -0,0 +1,8 @@
+function handleProductSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    console.log(data);
+}
